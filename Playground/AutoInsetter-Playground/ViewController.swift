b4ba2f7ffccda09c45a7049b5140c00ce0bb72be
Merrick Sapsford <merrick@sapsford.tech>
2018-10-11 08:53:15 +0200

    Add Playground project

---
+++ Playground/AutoInsetter-Playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoInsetter-Playground
+//
+//  Created by Merrick Sapsford on 11/10/2018.
+//  Copyright © 2018 UI At Six. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
