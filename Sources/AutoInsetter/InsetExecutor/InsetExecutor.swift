f8aa4464cebe5d6dd7c9870e908b5a7e55cefad2
Merrick Sapsford <merrick@sapsford.tech>
2019-03-24 10:00:58 +0100

    Improve logging

--- Sources/AutoInsetter/InsetExecutor/InsetExecutor.swift
+++ Sources/AutoInsetter/InsetExecutor/InsetExecutor.swift
@@ -29,13 +29,13 @@ internal class InsetExecutor {
         // If content inset has changed
         if let contentInset = calculator.calculateContentInset(from: spec, store: store) {
             view.contentInset = contentInset.new
-            print("[AutoInsetter] - Updated contentInset: \(view.contentInset)")
+            log("Updated contentInset: \(view.contentInset)")
             invalidateLayoutIfNeeded()
 
             // If content offset has changed
             if let contentOffset = calculator.calculateContentOffset(from: contentInset, store: store) {
                 view.contentOffset = contentOffset.new
-                print("[AutoInsetter] - Updated contentOffset: \(view.contentOffset)")
+                log("Updated contentOffset: \(view.contentOffset)")
             }
         }
         
